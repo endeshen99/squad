ee20d971752e9288cf152e5c0cebab0ee08df4cd
Anderson <anderson@DN0a1f62a5.SUNet>
2020-03-03 01:41:07 -0800

    test-speed

---
+++ tempppppp.py
@@ -0,0 +1,14 @@
+import time
+from transformers import *
+import torch
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased')
+
+# tokenizer = AlbertTokenizer.from_pretrained('albert-base-v1')
+# model = AlbertModel.from_pretrained('albert-base-v1')
+
+start_time = time.time()
+model(torch.randint(0, 30000, (4, 289)))
+end_time = time.time()
+print("Total execution time: {} seconds".format(end_time - start_time))
